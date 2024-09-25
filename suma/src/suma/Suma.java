03d2ef09f30d163668cfb4f3e5ace8d7343f1098
fernandopedridodev <fernandopedridodev@gmail.com>
2024-09-25 12:08:46 +0200

    Add files via upload

---
+++ suma/src/suma/Suma.java
@@ -0,0 +1,18 @@
+package suma;
+
+/**
+ *
+ * @author fernando.pedridomarino
+ */
+public class Suma {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        // sumamos as variables
+            int sum = a + b;
+            // resultado da suma
+              System.out.println("O resultado da suma é:" + sum);
+    }
+    
+}
