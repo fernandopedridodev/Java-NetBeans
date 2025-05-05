6ad6541835c7875837bdfc8f746cc7098a228bc2
fernandopedridodev <fernandopedridodev@gmail.com>
2025-05-05 09:50:20 +0200

    Add files via upload

--- Simulacro3av/src/simulacro3av/IncidenceDB.java
+++ Simulacro3av/src/simulacro3av/IncidenceDB.java
@@ -8,15 +8,27 @@ import java.util.ArrayList;
 
 /**
  * Gardamos as incidencias nunha clase de persistencia cun Arraylist
+ *
  * @author fernando.pedridomarino
  */
 public class IncidenceDB {
-       private static ArrayList<Incidence> incidences = new ArrayList<>();
+
+    private static ArrayList<Incidence> incidences = new ArrayList<>();
 
     public static ArrayList<Incidence> findByUser(String username) {
         ArrayList<Incidence> result = new ArrayList<>();
         for (Incidence inc : incidences) {
             if (inc.getSender().getUsername().equals(username)) {
+                result.add(inc);
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Incidence> findByStatus(int status) {
+        ArrayList<Incidence> result = new ArrayList<>();
+        for (Incidence inc : incidences) {
+            if (inc.getSender().equals(status)) {
                 result.add(inc);
             }
         }
@@ -30,7 +42,7 @@ public class IncidenceDB {
 
     public static void update(Incidence incidence) {
         // Neste exemplo, os obxectos están referenciados directamente, polo que non é necesario facer nada
-        // Se fose unha base de datos real, aquí faríase o update.
+       
     }
 
 }
